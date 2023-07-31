56589f3677e02db05f1df45ebd5c7eee954e6c8b
George-Kibe <georgekibewambui@gmail.com>
2023-07-31 14:19:27 +0300

    split settings

---
+++ core/core/utils/collections.py
@@ -0,0 +1,17 @@
+def deep_update(base_dict, update_with):
+    # iterate over each item in the new dict(Update_with)
+    for key, value in update_with.items():
+        # if the value is a dict
+        if isinstance(value, dict):
+            base_dict_value = base_dict.get(key)
+            #if the original value is also a dict then run through this same function
+            if isinstance(base_dict_value, dict):
+                deep_update(base_dict_value, value)
+            # if the original value is not a dictionary
+            else:
+                base_dict[key] = value
+        # if the new name is not a dictionary
+        else:
+            base_dict[key] = value
+    
+    return base_dict
